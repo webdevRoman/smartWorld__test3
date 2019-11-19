6c69bb335991e0ab002727dfae4e0ccbb474020d
Roman <rgrabelnikov@yandex.ru>
2019-11-19 15:50:33 +0700

    created project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,33 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Header></Header>
+    <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import Header from './components/Header'
+export default {
+  components: {
+    Header
   }
 }
+</script>
+
+<style lang="sass">
+body
+  margin: 0
+  font-family: "Arial", sans-serif
+.btn
+  padding: 10px 25px
+  border: none
+  background-color: #2980b9
+  border-radius: 10px
+  font-size: 16px
+  color: #fff
+  text-decoration: none
+  cursor: pointer
+  transition: 0.3s
+  &:hover
+    background-color: darken(#2980b9, 10)
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="header">
+    <div class="header-right">
+      <div class="header-logo">ToDo List</div>
+      <div class="header-username">"username"</div>
+    </div>
+    <router-link class="btn header-btn" to="/login">Выйти</router-link>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="sass">
+.header
+  padding: 20px 40px
+  background-color: #2c3e50
+  display: flex
+  justify-content: space-between
+  align-items: center
+  font-size: 18px
+  color: #fff
+  &-right
+    display: flex
+    justify-content: flex-start
+    align-items: center
+  &-logo
+    margin-right: 30px
+</style>
